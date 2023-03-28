dc2ffd69bbf202b15656816e32974d2eae6c6d60
Tracy-miranja <barazatracy44@gmail.com>
2023-03-28 16:24:12 +0300

    Added the buttons to my Calculator.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Math magician</h1>
     </div>
   );
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,27 @@
+import React from 'react';
+
 const KeyPad = () => (
-  <div />
+  <div className="Keys">
+    <button type="button">AC</button>
+    <button type="button">+/-</button>
+    <button type="button">%</button>
+    <button type="button" className="Orange">&divide;</button>
+    <button type="button">7</button>
+    <button type="button">8</button>
+    <button type="button">9</button>
+    <button type="button" className="Orange">&times;</button>
+    <button type="button">4</button>
+    <button type="button">5</button>
+    <button type="button">6</button>
+    <button type="button" className="Orange">-</button>
+    <button type="button">1</button>
+    <button type="button">2</button>
+    <button type="button">3</button>
+    <button type="button" className="Orange">+</button>
+    <button type="button" className="Zero">0</button>
+    <button type="button">.</button>
+    <button type="button" className="Orange">=</button>
+  </div>
 );
+
+export default KeyPad;
