6ae50171ac5e19eeefd1e78a5be3fff073a1c4ec
Tracy-miranja <barazatracy44@gmail.com>
2023-03-31 09:05:30 +0300

    Add the quote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Display from './components/Calculator';
+import Apps from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <h1>Math magician</h1>
       <Display />
+      <Apps />
     </div>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,49 @@
 import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as id } from 'uuid';
 
-function Quotes() {
+const loadingMessage = <p>Waiting for data loading....</p>;
+const errorMessage = <p>Oops, something went wrong!!!</p>;
+
+const Apps = () => {
   const [quotes, setQuotes] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const quotesApi = async () => {
+    setIsLoading(true);
+    try {
+      fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+        headers: {
+          'X-Api-Key': 'FSvXTjxiPe8a+r73Jt+dLQ==j1LfG5Zfc7QrFBl8',
+        },
+      })
+        .then((Response) => Response.json())
+        .then((data) => {
+          setQuotes(data);
+        });
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
+    quotesApi();
+  }, [setQuotes, setIsLoading]);
+  return (
+    <div>
+      {isLoading ? loadingMessage : null}
+      {hasError ? errorMessage : null}
+      {quotes && Array.isArray(quotes)
+        && quotes.map((data) => (
+          <div key={id()}>
+            <h1>{data.quote}</h1>
+            <h3>{data.author}</h3>
+          </div>
+        ))}
+    </div>
+  );
+};
 
-  });
-}
+export default Apps;
