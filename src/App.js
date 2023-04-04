a4bba54447148bc02975b3f716c981c0114424e0
Tracy-miranja <barazatracy44@gmail.com>
2023-04-04 23:25:34 +0300

    Add links to my website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import Display from './components/Calculator';
 import Apps from './components/Quote';
 import Home from './components/Home';
 
 function App() {
   return (
-
     <div className="App">
-      <Home />
-      <Apps />
-      <Display />
-
+      <nav>
+        <ul>
+          <li className="lnks"><Link to="/Calculator">Calculator</Link></li>
+          <li className="lnks"><Link to="/Quotes">Quotes</Link></li>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Quotes" element={<Apps />} />
+        <Route path="/Calculator" element={<Display />} />
+      </Routes>
     </div>
 
   );
