239389f704327a4884d15e8790c11acbfd861ccc
Tracy-miranja <barazatracy44@gmail.com>
2023-04-06 14:19:38 +0300

    Add test for claculate

---
+++ src/components/test/calculate.test.js
@@ -0,0 +1,24 @@
+import calculate from '../../logic/calculate';
+
+describe('calculate', () => {
+  test('should return null when the pressed button is AC', () => {
+    const buttonName = 'AC';
+    const expected = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    const result = calculate({ total: '123', next: '456', operation: '+' }, buttonName);
+    expect(result).toEqual(expected);
+  });
+  test('it should return total sum when the pressed button +', () => {
+    const buttonName = '+';
+    const expected = {
+      next: null,
+      operation: '+',
+      total: '60',
+    };
+    const result = calculate({ total: '40', next: '20', operation: '+' }, buttonName);
+    expect(result).toEqual(expected);
+  });
+});
